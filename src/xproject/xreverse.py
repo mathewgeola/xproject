a4e73cf36a49bce2517259ccb950c921f4ad60cf
mathewgeola <mathewgeola@gmail.com>
2025-08-19 17:40:05 +0800

    version = "0.1.5"

--- src/xproject/xreverse.py
+++ src/xproject/xreverse.py
@@ -3,7 +3,7 @@ import binascii
 from collections.abc import Buffer
 from typing import Literal, cast
 
-from Crypto.Cipher import AES
+from Crypto.Cipher import AES as AES_
 from Crypto.Util.Padding import pad
 
 
@@ -26,48 +26,62 @@ def hex_to_string(hex_string: str) -> str:
     return "".join(chr(int(pair[2:], 16)) for pair in hex_pairs if pair.startswith("\\x"))
 
 
-def aes_encrypt(
-        data: str,
-        key: str,
-        iv: str | None = None,
-        mode: Literal[1, 2] = AES.MODE_ECB,
-        style: Literal["pkcs7", "x923", "iso7816"] = "pkcs7",
-        fmt: Literal["base64", "hex"] = "base64"
-) -> str:
-    """
-    default: AES ECB PKCS7Padding
+class AES:
+    MODE_ECB = AES_.MODE_ECB
+    MODE_CBC = AES_.MODE_CBC
 
-    """
-    data_bytes: bytes = data.encode()
-    key_bytes: bytes = key.encode()
-    if iv is not None:
-        iv_bytes = iv.encode()
-    else:
-        iv_bytes = None
+    block_size = AES_.block_size
 
-    if mode == AES.MODE_ECB:
-        cipher = AES.new(key_bytes, mode)
-    else:
-        cipher = AES.new(key_bytes, mode, iv_bytes)
+    new = AES_.new
 
-    padded_data_bytes: bytes = pad(data_bytes, AES.block_size, style)
-    encrypted_data_bytes: bytes = cipher.encrypt(padded_data_bytes)
-    encrypted_data_buffer: Buffer = cast(Buffer, encrypted_data_bytes)
+    @staticmethod
+    def encrypt(
+            data: str,
+            key: str,
+            iv: str | None = None,
+            mode: Literal[1, 2] = MODE_ECB,
+            style: Literal["pkcs7", "x923", "iso7816"] = "pkcs7",
+            fmt: Literal["base64", "hex"] = "base64"
+    ) -> str:
+        """
+        default: AES ECB PKCS7Padding
 
-    if fmt == "base64":
-        encrypted_data: str = base64.b64encode(encrypted_data_buffer).decode()
-    elif fmt == "hex":
-        encrypted_data: str = binascii.hexlify(encrypted_data_buffer).decode()
-    else:
-        raise ValueError(
-            f"Invalid type for 'fmt': "
-            f"Expected ` Literal[\"base64\", \"hex\"] `, "
-            f"but got {type(fmt).__name__!r} (value: {fmt!r})"
-        )
-    return encrypted_data
+        """
+        data_bytes: bytes = data.encode()
+        key_bytes: bytes = key.encode()
+        if iv is not None:
+            iv_bytes = iv.encode()
+        else:
+            iv_bytes = None
 
+        if mode == AES.MODE_ECB:
+            cipher = AES.new(key_bytes, mode)
+        else:
+            cipher = AES.new(key_bytes, mode, iv_bytes)
+
+        padded_data_bytes: bytes = pad(data_bytes, AES.block_size, style)
+        encrypted_data_bytes: bytes = cipher.encrypt(padded_data_bytes)
+        encrypted_data_buffer: Buffer = cast(Buffer, encrypted_data_bytes)
+
+        if fmt == "base64":
+            encrypted_data: str = base64.b64encode(encrypted_data_buffer).decode()
+        elif fmt == "hex":
+            encrypted_data: str = binascii.hexlify(encrypted_data_buffer).decode()
+        else:
+            raise ValueError(
+                f"Invalid type for 'fmt': "
+                f"Expected ` Literal[\"base64\", \"hex\"] `, "
+                f"but got {type(fmt).__name__!r} (value: {fmt!r})"
+            )
+        return encrypted_data
+
+
+__all__ = [
+    "string_to_hex", "hex_to_string",
+    "AES",
+]
 
 if __name__ == '__main__':
-    print(aes_encrypt("123456", "1234567890123456"))
-    print(aes_encrypt("123456", "1234567890123456", "1234567890123456", mode=AES.MODE_CBC))
-    print(aes_encrypt("123456", "1234567890123456", "1234567890123456", mode=AES.MODE_ECB, style="x923"))
+    print(AES.encrypt("123456", "1234567890123456"))
+    print(AES.encrypt("123456", "1234567890123456", "1234567890123456", mode=AES.MODE_CBC))
+    print(AES.encrypt("123456", "1234567890123456", "1234567890123456", mode=AES.MODE_ECB, style="x923"))
